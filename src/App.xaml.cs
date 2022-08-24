728065987358840c8194d6f382535e34f7f34c46
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2022-08-24 02:28:04 +0100

    Created new project files

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Navigation;
-using OBS_Remote_Controls.WPF;
-using OBSWebsocketDotNet;
 
-namespace OBS_Remote_Controls
+namespace OBSRemoteControls
 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
     public partial class App : Application
     {
-        private Program app;
-
-        private void Application_Startup(object sender, StartupEventArgs args)
+        private void Application_Startup(object sender, StartupEventArgs e)
         {
-            app = new Program(args);
+            new MainWindow().ShowDialog();
         }
     }
 }
---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using WPFTemplate.Controls;
+using WPFTemplate.Styles;
+
+namespace OBSRemoteControls
+{
+    public partial class MainWindow : WindowChrome
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StylesManager.onChange += StylesManager_onChange;
+            //Trigger the styles to be updated here to override the default values.
+            StylesManager_onChange();
+        }
+
+        private void StylesManager_onChange()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                Foreground = StylesManager.foreground;
+                Background = StylesManager.background;
+                BackgroundAlt = StylesManager.backgroundAlt;
+                Accent = StylesManager.accent;
+            });
+        }
+    }
+}
